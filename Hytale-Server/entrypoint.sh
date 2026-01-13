4e5467d261938375af0694c357c4f16f735392f1
AceOfKestrels <git@kestrels.dev>
2026-01-13 18:20:30 +0100

    Inital commit

---
+++ Hytale-Server/entrypoint.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+if [ -d /opt/hytale-server ] && [ -n "$(ls -A /opt/hytale-server)" ]; then
+    exec java -jar /opt/hytale-server/Server/HytaleServer.jar --assets /opt/hytale-server/Assets.zip
+fi
+
+wget https://downloader.hytale.com/hytale-downloader.zip && \
+    unzip hytale-downloader.zip -d hytale-downloader && \
+    rm hytale-downloader.zip
+
+./hytale-downloader/hytale-downloader-linux-amd64 && \
+    unzip ./*.zip -d /opt/hytale-server
+
+exec java -jar /opt/hytale-server/Server/HytaleServer.jar --assets /opt/hytale-server/Assets.zip
