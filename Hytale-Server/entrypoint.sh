dfbeec76691863113d4f53e6f8539b297d545294
AceOfKestrels <git@kestrels.dev>
2026-01-13 18:39:12 +0100

    Fix for restart

--- Hytale-Server/entrypoint.sh
+++ Hytale-Server/entrypoint.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/env bash
 
+if ! [ -d /opt/hytale-server-data ]; then
+    mkdir /opt/hytale-server-data
+fi
+
 if [ -d /opt/hytale-server ] && [ -n "$(ls -A /opt/hytale-server)" ]; then
+    cd /opt/hytale-server-data
     exec java -jar /opt/hytale-server/Server/HytaleServer.jar --assets /opt/hytale-server/Assets.zip
 fi
 
@@ -11,9 +16,5 @@ wget https://downloader.hytale.com/hytale-downloader.zip && \
 ./hytale-downloader/hytale-downloader-linux-amd64 && \
     unzip ./*.zip -d /opt/hytale-server
 
-if ! [ -d /opt/hytale-server-data ]; then
-    mkdir /opt/hytale-server-data
-fi
 cd /opt/hytale-server-data
-
 exec java -jar /opt/hytale-server/Server/HytaleServer.jar --assets /opt/hytale-server/Assets.zip
